4c131ba1fe1ce1b45629b5ab8a6158c42dfcd726
Aris <arismouz3@gmail.com>
2021-03-20 23:13:53 +0200

    Print file contents

--- src/App.java
+++ src/App.java
@@ -1,8 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int x=0;
-        int y=0;
-        int z=0;
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter filename to open!");
+        String filename = input.next();
+        try
+        {
+            Scanner inFile = new Scanner(new File(filename));
+
+            while (inFile.hasNext())
+            {
+                String word = inFile.next();
+
+                System.out.println(word);
+            }
+
+            inFile.close();
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
